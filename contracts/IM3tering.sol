349594b4a89e80dec5e0219fd6496b562c89ef78
ichristwin <ichristwin@gmail.com>
2023-09-21 18:37:57 +0100

    minor

--- contracts/IM3tering.sol
+++ contracts/IM3tering.sol
@@ -59,5 +59,5 @@ interface IM3tering {
 
     function stateOf(uint256 tokenId) external view returns (bool);
 
-    function tariffOf(uint256 tokenId) external view returns (uint);
+    function tariffOf(uint256 tokenId) external view returns (uint256);
 }
