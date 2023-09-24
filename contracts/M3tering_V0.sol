62898470b72c9a0b2dab8daa3133e58139f171b4
ichristwin <ichristwin@gmail.com>
2023-09-24 16:46:16 +0100

    added interfaces dir

--- contracts/M3tering_V0.sol
+++ contracts/M3tering_V0.sol
@@ -2,10 +2,10 @@
 pragma solidity ^0.8.19;
 
 import "./protocol-abc/Protocol.sol";
-import "./IM3tering_V0.sol";
+import "./interfaces/IM3tering_V0.sol";
 
 /// @custom:security-contact info@whynotswitch.com
-contract M3tering_V0 is IM3tering_V0, Protocol {
+contract M3tering_V0 is Protocol, IM3tering_V0 {
     function claim() external whenNotPaused {
         uint256 amount = revenues[msg.sender];
         if (amount < 1) revert InputIsZero();
