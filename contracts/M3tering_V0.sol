385c077d974f40c3ddbedddd84a4e27efd10b5c0
ichristwin <ichristwin@gmail.com>
2023-09-24 16:38:25 +0100

    undo

--- contracts/M3tering_V0.sol
+++ contracts/M3tering_V0.sol
@@ -5,7 +5,7 @@ import "./protocol-abc/Protocol.sol";
 import "./IM3tering_V0.sol";
 
 /// @custom:security-contact info@whynotswitch.com
-contract M3tering_V0 is IM3tering_V0, Protocol() {
+contract M3tering_V0 is IM3tering_V0, Protocol {
     function claim() external whenNotPaused {
         uint256 amount = revenues[msg.sender];
         if (amount < 1) revert InputIsZero();
