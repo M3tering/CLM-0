5b8640b78f9076f607d637e0564ae52d47f42639
ichristwin <ichristwin@gmail.com>
2023-11-25 01:43:18 +0100

    fixed issue with lib

--- Strategy_V0.sol
+++ Strategy_V0.sol
@@ -4,10 +4,10 @@ pragma solidity ^0.8.19;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.3/contracts/token/ERC20/IERC20.sol";
 
 /// @custom:security-contact info@whynotswitch.com
-library Strategy_V0 {
+contract Strategy_V0 {
     error TransferError();
 
     function claim(uint256 revenueAmount, address receiver, uint256 outputAmount, uint256 deadline) public {
-        if (!IERC20(0x1CbAd85Aa66Ff3C12dc84C5881886EEB29C1bb9b).transfer(receiver, revenueAmount)) revert TransferError();        
+        if (!IERC20(0xd9145CCE52D386f254917e481eB44e9943F39138).transferFrom(msg.sender, receiver, revenueAmount)) revert TransferError();
     }
 }
