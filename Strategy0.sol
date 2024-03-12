76e294edb639fbb8ecccdfd02e189eac1bac2487
ichristwin <ichristwin@gmail.com>
2024-03-12 02:55:05 +0100

    changed from ERC20 to native ether

--- Strategy0.sol
+++ Strategy0.sol
@@ -1,18 +1,16 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.24;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.3/contracts/token/ERC20/IERC20.sol";
 import "./interfaces/IStrategy.sol";
 
 /// @custom:security-contact info@whynotswitch.com
 contract Strategy0 is IStrategy {
     error TransferError();
 
-    IERC20 public constant DAI =
-        IERC20(0xd9145CCE52D386f254917e481eB44e9943F39138);
-
-    function claim(uint256 amount, bytes calldata data) external {
+    function claim(bytes calldata data) external payable {
         address to = abi.decode(data, (address));
-        if (!DAI.transferFrom(msg.sender, to, amount)) revert TransferError();
+
+        (bool sent, ) = to.call{value: msg.value}("");
+        if (!sent) revert TransferError();
     }
 }
